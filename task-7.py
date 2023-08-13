a77f63b29bfb7b0ad7de81f04d8618f6534e3f72
yazeed1993 <yazeed753@gmail.com>
2023-08-13 10:33:15 +0300

    add functions to the file

--- task-7.py
+++ task-7.py
@@ -1,67 +1,95 @@
 #######
 # Q 1 #
 #######
-# rows = 6
-# for xx in range(1, rows):
-#     for i in range(1, xx + 1):
-#         print(i, end=' ')
-#     print("")
+def num_row():
+    rows = 6
+    for xx in range(1, rows):
+        for i in range(1, xx + 1):
+            print(i, end=' ')
+        print("")
 
+
+num_row()
 #######
 # Q 2 #
 #######
-# number = int(input("Enter number: "))
-# sum = 0
-# for i in range(1, number + 1):
-#     sum += i
-# print('sum is :  ' + str(sum))
+def num_calc():
+    number = int(input("Enter number: "))
+    sum = 0
+    for i in range(1, number + 1):
+        sum += i
+    print('sum is :  ' + str(sum))
 
+num_calc()
 #######
 # Q 3 #
 #######
+def prnt_name():
+    l0 = input('enter your name ')
+    for i in range(0, 7):
+        for j in l0[0:i + 1]:
+            print(j, end='')
+        print()
+    for ii in range(0, 7):
+        for jj in l0[0:-1 - ii]:
+            print(jj, end='')
+        print()
 
-# l0 = input('enter your name ')
-# for i in range(0,7):
-#     for j in l0[0:i+1]:
-#         print(j, end='')
-#     print()
-# for ii in range(0,7):
-#     for jj in l0[0:-1-ii]:
-#         print(jj, end='')
-#     print()
+prnt_name()
+
 
 #######
 # Q 4 #
 #######
-# l0 = input('enter your name ')
-# x=l0[::-1]
-# print(x)
+def rev_name():
+    l0 = input('enter your name ')
+    x = l0[::-1]
+    print(x)
+
+rev_name()
+
 
 #######
 # Q 5 #
 #######
-# num = int(input("Enter Range: "))
-# i = num
-# while ( i >= 1):
-#     print (i, end = ' ')
-#     i = i - 1
+
+def num_rev_range():
+    num = int(input("Enter Range: "))
+    i = num
+    while (i >= 1):
+        print(i, end=' ')
+        i = i - 1
+
+
+num_rev_range()
 
 #######
 # Q 6 #
 #######
-# for i in range(10):
-#     print((i + 1) * 5, end=' ')
+
+def num_multi():
+    for i in range(10):
+        print((i + 1) * 5, end=' ')
+
+
+num_multi()
 
 #######
 # Q 7 #
 #######
 def num1():
-    n0 = input('Enter the Limit_Number: ')
-    n1 = int(input('Enter the maximum outputs to display (Max display_on_screen): '))
-    n2 = int(input('Enter the Target_Number : '))
-    for x in n0:
-        for i in range(n1):
-            print((i + 1) * n2, end=' ')
+    limit = int(input('Enter the Limit_Number: '))
+    maxi = int(input('Enter the maximum outputs to display (Max display_on_screen): '))
+    target = int(input('Enter the Target_Number : '))
+
+    for i in range(maxi):
+
+        res = (i + 1) * target
+        if res >= limit:
+            break
+        print(res, end=' ')
+
+
 num1()
 
 
